87f11a176928ee476426e2c04a82adb1f9527794
Jarek Potiuk <jarek.potiuk@polidea.pl>
2011-01-23 21:34:18 +0100

    Initial commit.

---
+++ src/pl/polidea/androidflip3d/Flip3dActivity.java
@@ -0,0 +1,15 @@
+package pl.polidea.androidflip3d;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Flip3dActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
